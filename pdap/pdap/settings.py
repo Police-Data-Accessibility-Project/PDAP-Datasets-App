c61fd0c5e210a94756d02c218d7a28d6d31a05b9
jlintag <jlintag@gmail.com>
2021-05-08 00:12:31 -0400

    change static root - jl

--- pdap/pdap/settings.py
+++ pdap/pdap/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS  = [
     BASE_DIR / "staticfiles"
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
