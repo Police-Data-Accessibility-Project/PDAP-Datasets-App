6c001dcf6b8cbcc9b0d24a504e6ea5ba7a1832d6
jlintag <jlintag@gmail.com>
2021-05-08 14:00:54 -0400

    ensure debug mode is pulled from env - jl

--- pdap/pdap/settings.py
+++ pdap/pdap/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = ['pdap.ericturner.it', '127.0.0.1', 'map.pdap.io', 'localhost', 'app.pdap.io', os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")]
 
